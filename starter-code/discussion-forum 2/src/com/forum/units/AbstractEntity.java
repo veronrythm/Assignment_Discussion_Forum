c726c6eaea9eff27facea69f5bc82601660b44d3
veronrythm <virenrdeshpande@gmail.com>
2021-01-19 10:03:59 +0530

    first commit

---
+++ starter-code/discussion-forum 2/src/com/forum/units/AbstractEntity.java
@@ -0,0 +1,53 @@
+package com.forum.units;
+
+import java.util.Date;
+
+import com.forum.util.Utility;
+
+public abstract class AbstractEntity {
+
+	private Date created;
+	private long id;
+
+	/**
+	 *
+	 * Write a method called getId that requires no parameters and
+	 * returns the id of this Abstract entity
+	 *
+	 * @return the id of this Abstract entity
+	 */
+	public long getId() {
+		return this.id;
+	}
+
+	/**
+	 *
+	 * Write a method called setId that takes in a parameter and
+	 * sets the id of this Abstract entity to the parameter.
+	 *
+	 * This method should return void.
+	 *
+	 * @param id: the id of this Abstract entity
+	 */
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	/**
+	 *
+	 * Write an abstract method called autoGenerateId.
+	 *
+	 * This method doesn't require any parameters and returns void
+	 *
+	 */
+	abstract void autoGenerateId();
+
+
+	public Date getCreated() {
+		return created;
+	}
+
+	public void setCreated() {
+		this.created = Utility.getCurrentDate();
+	}
+}
